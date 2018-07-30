4d55783f7061675e2e468f82fe05988c976e0c1d
zhougongkai <617947483@qq.com>
2018-07-30 17:02:28 +0800

    添加新功能

---
+++ src/main/java/com/summary/zkhdsummary/controller/SummaryController.java
@@ -0,0 +1,19 @@
+package com.summary.zkhdsummary.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SummaryController {
+
+    /**
+     * 访问首页
+     * @return
+     */
+    @RequestMapping(value = {"/","/index"})
+    public String index(){
+
+        return "index";
+    }
+}
--- src/test/java/com/summary/zkhdsummary/ZkhdsummaryApplicationTests.java
+++ src/test/java/com/summary/zkhdsummary/ZkhdsummaryApplicationTests.java
@@ -1,16 +1,25 @@
 package com.summary.zkhdsummary;
 
+import com.summary.zkhdsummary.bean.power;
+import com.summary.zkhdsummary.mapper.powerMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZkhdsummaryApplicationTests {
-
+    @Autowired
+    private powerMapper powerMapper;
     @Test
     public void contextLoads() {
+
+        power power = powerMapper.selectByPrimaryKey(1);
+
+        System.out.println(power.getPower());
+
     }
 
 }
